693d95ff56e26b62995790315a74a81a94034fa1
akriti242 <arokritika278@gmail.com>
2024-01-25 21:51:11 +0530

    first commit

---
+++ DOM/get.js
@@ -0,0 +1,18 @@
+alert("hello kritika");
+
+//let data = document.getElementById('a').innerText;
+
+// let data = document.getElementById('a').innerHTML;
+
+// let data = document.getElementById('aa').innerHTML;
+
+//let data = document.getElementById('a').getAttribute("style");
+
+let data = document.getElementById('a').getAttributeNode("style");
+
+// let data = document.getElementById('a').inneattributes[0];
+
+
+
+
+console.log(data);
---
+++ DOM/set.js
@@ -0,0 +1,6 @@
+alert("hello");
+
+document.getElementById("pn").innerHTML = "<center><h2>PNINFOSY Gwalior 1234</h2></center>";
+document.getElementById("pn").setAttribute("style","border:10px dotted red");
+//document.getElementById("pn").removeAttribute("style");
+document.getElementById("s").innerHTML = "<h1>GWALIOR</h1>";
